20d63692e12847487bbadba1a8d4d496f0049905
gzp <zswangzp@163.com>
2017-08-23 18:08:48 +0800

    读取json文件,命名空间

--- SinaWeibo/SinaWeibo/Classes/Main(主要)/STWTabBarController.swift
+++ SinaWeibo/SinaWeibo/Classes/Main(主要)/STWTabBarController.swift
@@ -12,14 +12,64 @@ class STWTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+//        addChildViewController("HomeViewController", title: "首页", imageName: "tabbar_home")
+//        addChildViewController("MessageViewController", title: "消息", imageName: "tabbar_message_center")
+//        addChildViewController("DiscoverViewController", title: "发现", imageName: "tabbar_discover")
+//        addChildViewController("ProfileViewController", title: "我的", imageName: "tabbar_profile")
 
-        addChildViewController(HomeViewController(), title: "首页", imageName: "tabbar_home")
-        addChildViewController(MessageViewController(), title: "消息", imageName: "tabbar_message_center")
-        addChildViewController(DiscoverViewController(), title: "发现", imageName: "tabbar_discover")
-        addChildViewController(ProfileViewController(), title: "我的", imageName: "tabbar_profile")
+        // 获取文件路径
+        guard let filePath = Bundle.main.path(forResource: "MainVCSettings.json", ofType: nil) else {
+            STWLog("没有找到MainVCSettings.json对应的文件")
+            return
+        }
+     
+        // 读取文件数据
+        guard let jsonData = try? Data(contentsOf: URL(fileURLWithPath: filePath)) else {
+            STWLog("读取二进制文件失败")
+            return
+        }
+
+        //将json数据转换成 数组格式
+        guard let json = try? JSONSerialization.jsonObject(with: jsonData, options: .mutableContainers) , let jsonArray = json as? [[String : AnyObject]] else {
+            STWLog("读取文件失败")
+            return
+        }
+        
+        jsonArray.forEach{
+            
+            guard let vcName = $0["vcName"] as? String else {
+                return
+            }
+            guard let title = $0["title"] as? String else {
+                return
+            }
+            guard let imageName = $0["imageName"] as? String else {
+                return
+            }
+            
+            addChildViewController(vcName, title: title, imageName: imageName)
+        
+        }
     }
     
-    func addChildViewController(_ childVc: UIViewController , title : String , imageName : String) {
+    func addChildViewController(_ childClassString: String , title : String , imageName : String) {
+        
+        //获取命名空间名称并且拼接字符串 然后转化成对应的Class类型
+       guard let nameSpace = Bundle.main.infoDictionary!["CFBundleExecutable"] as? String , let childClass = NSClassFromString(nameSpace + "." + childClassString) else
+       {
+         STWLog("没有获取到字符串对应的Class")
+         return
+       }
+        
+        // 将对应的anyclass类型转换成控制器类型
+      guard  let childVcType = childClass as? UIViewController.Type else
+      {
+        STWLog("没有获取到对应控制器的类型")
+        return
+      }
+        
+        //将childVcType 转换成对应的控制器
+       let childVc = childVcType.init()
         
         //设置自控制的属性
         childVc.title =  title
